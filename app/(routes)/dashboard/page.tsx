8038862074f049dff1cb15d0bd655b4b01d079c5
DoDucLong15 <llong6412@gmail.com>
2025-02-22 22:32:54 +0700

    feat: add business creation

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,35 @@
 "use client";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import { app } from "@/config/FirebaseConfig";
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
+  const db = getFirestore(app);
+  const {user} = useKindeBrowserClient(); 
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => { 
+    user && isBusinessRegistered();
+  }, [user]);
+
+  const isBusinessRegistered = async () => {
+    const docRef = doc(db, "Business", user.email);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      console.log("No such document!");
+      router.replace('/create-business');
+    }
+    setLoading(false);
+  }
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div>
       Dashboard
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Outfit({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
         className={inter.className}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
