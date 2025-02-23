a33e74d6509d86d137d7770b7f677d1fdc275dc9
DoDucLong15 <llong6412@gmail.com>
2025-02-24 00:00:24 +0700

    feat: implement meeting event list and enhance meeting type page layout

--- app/(routes)/dashboard/meeting-type/page.tsx
+++ app/(routes)/dashboard/meeting-type/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import MeetingEventList from "./_components/MeetingEventList";
+
 function MeetingType() {
   return (
-    <div>MeetingType</div>
-  )
+    <div className="p-5">
+      <div className="flex flex-col gap-5">
+        <h2 className="font-bold text-3xl">Meeting Event Type</h2>
+        <Input placeholder="Search" className="max-w-xs" />
+        <hr></hr>
+      </div>
+
+      <MeetingEventList />
+    </div>
+  );
 }
-export default MeetingType
+export default MeetingType;
